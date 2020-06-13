0a07ca4f1c08d4520c5446569740435e3f87ae22
Dylan Smit <dylan.smit@hotmail.com>
2020-06-14 01:46:11 +0200

    Added movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,18 +21,77 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        switch (movedir)
+            if (Input.GetKeyDown(KeyCode.W))
         {
+            movedir = MoveDirection.Up;
+            move = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movedir = MoveDirection.Down;
+            move = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movedir = MoveDirection.Right;
+            move = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movedir = MoveDirection.Left;
+            move = true;
+        }
+        switch (movedir)
+        {    
             case MoveDirection.none:
                 break;
             case MoveDirection.Up:
                 if (move)
                 {
-                    targetLocation = new Vector2(transform.position.x, transform.position.y + 1);
+                    targetLocation = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y + 1f));
                     move = false;
                 }
-                transform.position = Vector2.MoveTowards(transform.position, targetLocation, 0.5f);
+                transform.position = Vector2.MoveTowards(transform.position, targetLocation, 0.1f);
                 if (transform.position.y >= targetLocation.y)
+                {
+                    transform.position = targetLocation;
+                    movedir = MoveDirection.none;
+                }
+                break;
+            case MoveDirection.Down:
+                if (move)
+                {
+                    targetLocation = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y - 1f));
+                    move = false;
+                }
+                transform.position = Vector2.MoveTowards(transform.position, targetLocation, 0.1f);
+                if (transform.position.y <= targetLocation.y)
+                {
+                    transform.position = targetLocation;
+                    movedir = MoveDirection.none;
+                }
+                break;
+            case MoveDirection.Right:
+                if (move)
+                {
+                    targetLocation = new Vector2(Mathf.RoundToInt(transform.position.x + 1f), Mathf.RoundToInt(transform.position.y));
+                    move = false;
+                }
+                transform.position = Vector2.MoveTowards(transform.position, targetLocation, 0.1f);
+                if (transform.position.x >= targetLocation.x)
+                {
+                    transform.position = targetLocation;
+                    movedir = MoveDirection.none;
+                }
+                break;
+            case MoveDirection.Left:
+                if (move)
+                {
+                    targetLocation = new Vector2(Mathf.RoundToInt( transform.position.x - 1f), Mathf.RoundToInt(transform.position.y));
+                    move = false;
+                }
+                transform.position = Vector2.MoveTowards(transform.position, targetLocation, 0.1f);
+                if (transform.position.x <= targetLocation.x)
                 {
                     transform.position = targetLocation;
                     movedir = MoveDirection.none;
@@ -46,5 +105,25 @@ public class Player : MonoBehaviour
     {
         movedir = MoveDirection.Up;
         move = true;
+    }
+    public void onDown()
+    {
+        movedir = MoveDirection.Down;
+        move = true;
+    }
+    public void onRight()
+    {
+        movedir = MoveDirection.Right;
+        move = true;
+    }
+    public void onLeft()
+    {
+        movedir = MoveDirection.Left;
+        move = true;
+    }
+
+    public void onHold()
+    {
+        move = false;
     }
 }
